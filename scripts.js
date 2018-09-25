1c017aeed4dca7a27e2c4c3af836a4cd6ce62377
Breeland Abrams <breeland.abrams@gmail.com>
2018-09-25 12:10:17 -0700

    Reformat about page and add transition script

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("a.scroll").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500
+    );
+  });
+});
